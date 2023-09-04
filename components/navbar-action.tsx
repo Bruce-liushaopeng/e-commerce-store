585613a3ea4c572cb9552901334afba348688810
bruceliu <bruceliu1107@gmail.com>
2023-09-04 18:15:03 +0800

    add-to-cart function working

--- components/navbar-action.tsx
+++ components/navbar-action.tsx
@@ -3,6 +3,7 @@
 import { ShoppingBag } from "lucide-react";
 import Button from "./ui/button";
 import { useEffect, useState } from "react";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
   // cart is going to saved use the local storage, that can cause hydration issue
@@ -13,6 +14,8 @@ const NavbarActions = () => {
     setIsMounted(true);
   });
 
+  const cart = useCart();
+
   if (!isMounted) {
     return null;
   }
@@ -21,7 +24,7 @@ const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white"> {cart.items.length} </span>
       </Button>
     </div>
   );
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,12 +8,14 @@ import Currency from "./currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const cart = useCart()
   const previewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
@@ -25,6 +27,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 
     previewModal.onOpen(data);
   };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  };
+
+  
 
   return (
     <div
@@ -45,7 +55,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={26} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={26} className="text-gray-600" />}
             />
           </div>
