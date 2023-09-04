8d1f3450a82b96bda1746c763b1850964bd28e2b
bruceliu <bruceliu1107@gmail.com>
2023-09-04 20:26:48 +0800

    display shopping cart item, delete from shopping cart

--- components/navbar-action.tsx
+++ components/navbar-action.tsx
@@ -4,6 +4,7 @@ import { ShoppingBag } from "lucide-react";
 import Button from "./ui/button";
 import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarActions = () => {
   // cart is going to saved use the local storage, that can cause hydration issue
@@ -14,6 +15,7 @@ const NavbarActions = () => {
     setIsMounted(true);
   });
 
+  const router = useRouter()
   const cart = useCart();
 
   if (!isMounted) {
@@ -22,7 +24,7 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white"> {cart.items.length} </span>
       </Button>
